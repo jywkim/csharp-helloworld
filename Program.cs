1f0de36473273446bfbf3c32e116dee5a5203864
jywkim <jkim589@gmail.com>
2018-08-23 22:01:20 -0400

    branches and loops

--- Program.cs
+++ Program.cs
@@ -73,25 +73,100 @@ namespace csharp_helloworld
           Console.WriteLine(third);
         }
 
+        static void WorkingWithFixedPoints()
+        {
+          decimal min = decimal.MinValue;
+          decimal max = decimal.MaxValue;
+          Console.WriteLine($"The range of the decimal type is {min} to {max}");
+          double a = 1.0;
+          double b = 3.0;
+          Console.WriteLine(a / b);
+          decimal c = 1.0M;
+          decimal d = 3.0M;
+          Console.WriteLine(c / d);
+          double radius = 2.50;
+          double area = Math.PI * radius * radius;
+          Console.WriteLine(area);
+        }
+
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+            int c = 4;
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
+            if ((a + b + c > 10) || (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }
+        }
+
+        static void WhileDoForLoops()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+            for(int index = 0; index < 10; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+        }
+
+        static void CombineBranchesAndLoops()
+        {
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+
         static void Main(string[] args)
         {
             // WorkingWithIntegers();
             // OrderPrecedence();
             // TestLimits();
             // WorkWithDoubles();
-
-            decimal min = decimal.MinValue;
-            decimal max = decimal.MaxValue;
-            Console.WriteLine($"The range of the decimal type is {min} to {max}");
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
-            decimal c = 1.0M;
-            decimal d = 3.0M;
-            Console.WriteLine(c / d);
-            double radius = 2.50;
-            double area = Math.PI * radius * radius;
-            Console.WriteLine(area);
+            // WorkingWithFixedPoints();
+            // ExploreIf();
+            // WhileDoForLoops();
+            CombineBranchesAndLoops();
         }
     }
 }
